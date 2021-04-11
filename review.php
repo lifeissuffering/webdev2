3822268654424906771ef65e4218821427f9eb63
moeimyageri@mail.ru <moeimyageri@mail.ru>
2021-04-11 14:26:23 +0300

    fix code

--- review.php
+++ review.php
@@ -30,7 +30,7 @@ include "head.html";
                 <? echo $author; echo ', '; echo $date?>
             </div>
             <? if ($video != ""){
-            echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $video?>" 
+            echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$video.'" 
                 title="YouTube video player" frameborder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
                 gyroscope; picture-in-picture" 
