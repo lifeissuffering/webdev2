41ffbfe4ccd1b88f5ad3285cb5b6bf6c9d629ec1
moeimyageri@mail.ru <moeimyageri@mail.ru>
2021-04-11 14:07:34 +0300

    edit db connection string

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 require "rb.php";
-R::setup( 'pgsql:host=localhost;dbname=webdev1',
-        'postgres', 'CampFord27' );
+R::setup( 'pgsql:host=ec2-54-73-68-39.eu-west-1.compute.amazonaws.com;dbname=d7r5hjcae94l52',
+        'uanemgremleemo', 'c2379916529d8c27b53569e968cb995b07f0c9297323145b4bbfe05d6afb8d4c' );
         
 session_start();
 ?>
